61e6297b85b81cd5771fec226fe2a87f0bc9914f
loross90 <loross90@gmail.com>
2022-01-12 23:26:00 +0300

    1

--- Requestor.py
+++ Requestor.py
@@ -17,13 +17,19 @@ uniqueSymDictForGraph = dict(zip(uniqueSym, list(range(len(uniqueSym)))))
 g = Graph()
 
 g.add_vertices(len(uniqueSymDictForGraph))
+g.vs["name"] = list(uniqueSymDictForGraph.keys())
 
 #g.add_edge(uniqueSymDictForGraph[interested[5]['baseAsset']],uniqueSymDictForGraph[interested[5]['quoteAsset']])
 
 for i in interested:
     g.add_edge(uniqueSymDictForGraph[i['baseAsset']],uniqueSymDictForGraph[i['quoteAsset']])
 
-g.shorte
-print(objects['symbols'][2]['quoteAsset'])
+path=g.get_shortest_paths("CREAM",to="USDT",mode=OUT,output='vpath')
+
+for n in path[0]:
+    print("{}".format(g.vs[n]['name']))
+
+#print(objects['symbols'][2]['quoteAsset'])
+
 
 #print(ans.content)
